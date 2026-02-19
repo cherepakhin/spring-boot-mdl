7a8e1443dca9f60740085b124f2979945cdac998
vasi <vasi.che@gmail.com>
2026-02-19 21:09:55 +0500

    add logger to AppWideExceptionHandler.java

--- src/main/java/material/design/exception/AppWideExceptionHandler.java
+++ src/main/java/material/design/exception/AppWideExceptionHandler.java
@@ -1,15 +1,18 @@
 package material.design.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class AppWideExceptionHandler {
-	
+	private static final Logger logger = LoggerFactory.getLogger(AppWideExceptionHandler.class);
 	@ExceptionHandler(UsernameAlreadyInUseException.class)	
 	public String usernameException(Model model) {
 		model.addAttribute("message", "username already in use, please choose another one");
+		logger.error("username already in use, please choose another one");
 		return "error/usernameException";
 	}
 
