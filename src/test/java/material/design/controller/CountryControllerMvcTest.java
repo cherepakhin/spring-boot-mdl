a7b0aea0ffb4e7c521cf99511659e9ee75bd9c41
vasi <vasi.che@gmail.com>
2026-02-20 02:44:56 +0500

    add test CountryControllerMvcTest.getCountryContentWithUser()

--- src/test/java/material/design/controller/CountryControllerMvcTest.java
+++ src/test/java/material/design/controller/CountryControllerMvcTest.java
@@ -10,10 +10,14 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.fail;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -41,7 +45,44 @@ public class CountryControllerMvcTest {
 
     @WithMockUser(value = "user")
     @Test
-    public void getCountryContent() throws Exception {
+    public void getCountryWithUser() throws Exception {
+        List<Country> countries = new ArrayList<>();
+        Country country = new Country();
+        country.setId(100L);
+        countries.add(country);
+        when(repository.findAll()).thenReturn(countries);
+        this.mockMvc.perform(get("/country")).andDo(print())
+                .andExpect(view().name("countries"));
+    }
+
+    @WithMockUser(value = "user")
+    @Test
+    public void getCountryContentWithUser() throws Exception {
+        List<Country> countries = new ArrayList<>();
+        Country country = new Country();
+        country.setId(100L);
+        countries.add(country);
+        when(repository.findAll()).thenReturn(countries);
+        ResultActions result = this.mockMvc.perform(get("/country"));
+        MvcResult mvcResult = result.andReturn();
+        Object countriesResult = mvcResult.getModelAndView().getModel().get("countries");
+
+        if (countriesResult instanceof List) {
+            if (!((List) countriesResult).isEmpty()) {
+                List<Country> countriesList = (List<Country>) countriesResult;
+                assertEquals(1, countriesList.size());
+
+                Country countryResult = countriesList.get(0);
+                assertEquals(100, countryResult.getId().longValue());
+            } else {
+                fail();
+            }
+        }
+    }
+
+    @WithMockUser(value = "fake")
+    @Test
+    public void getCountryContentWithFakeUser() throws Exception {
         this.mockMvc.perform(get("/country")).andDo(print())
                 .andExpect(view().name("countries"));
     }
