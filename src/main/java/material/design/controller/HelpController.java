a4a534fe85444270d32c53796a48df5a1ba42ef9
vasi <vasi.che@gmail.com>
2026-02-19 21:11:01 +0500

    add logger to HelpController.java.java

--- src/main/java/material/design/controller/HelpController.java
+++ src/main/java/material/design/controller/HelpController.java
@@ -1,5 +1,7 @@
 package material.design.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,9 +10,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/")
 public class HelpController {
-	
+	private static final Logger logger = LoggerFactory.getLogger(HelpController.class);
+
 	@GetMapping("/help")
 	public String help(Model model) {
+		logger.info("Go to help");
 		return "help";
 	}
 }
