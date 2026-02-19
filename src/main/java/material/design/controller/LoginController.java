1280cd67c9151b0d06b91b89f4179f76cec82a1a
vasi <vasi.che@gmail.com>
2026-02-19 21:11:36 +0500

    add logger to LoginController.java

--- src/main/java/material/design/controller/LoginController.java
+++ src/main/java/material/design/controller/LoginController.java
@@ -1,5 +1,7 @@
 package material.design.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +10,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/login")
 public class LoginController {
 
+    private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
+
     @GetMapping("")
     public String login() {
+        logger.info("Login()");
         return "login";
     }
 
